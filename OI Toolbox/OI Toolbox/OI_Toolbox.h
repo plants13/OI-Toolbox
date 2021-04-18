9cc48967def44fe3493b1fa25dca9eeb25cdcea2
plants13 <plants13@163.com>
2021-04-18 12:05:37 +0800

    WIP

---
+++ OI Toolbox/OI Toolbox/OI_Toolbox.h
@@ -0,0 +1,16 @@
+#ifndef OI_Toolbox
+#define OI_Toolbox
+#include <iostream>
+#include <cstdio>
+#include <cstring>
+#include <string>
+#include <cmath>
+#include <cstdlib>
+#include <algorithm>
+#include <fstream>
+#include <stack>
+#include <vector>
+#include <cctype>
+#include <ctime>
+#include <Windows.h>
+#endif
